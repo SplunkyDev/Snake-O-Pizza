22ef25f0b6e0eb6b597c46f535747fb0b6627fc8
DevJeeth <konerirajb@yahoo.com>
2020-05-26 22:55:31 +0530

    +Animation with blening added with controls

---
+++ Assets/0_Snake_O_Pizza/0_Scripts/GameMechanics/PlayerAnimator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour, IAnimation
+{
+	public Animator AnimPlayer { get; private set; }
+	private IPlayerInput m_refPlayerInput;
+
+	void Start()
+    {
+		AnimPlayer = GetComponent<Animator>();
+		m_refPlayerInput = GetComponent<IPlayerInput>();
+
+	}
+
+    void Update()
+    {
+		SetVelue();
+    }
+
+	public void SetVelue()
+	{
+		AnimPlayer.SetFloat("TurnBlend", m_refPlayerInput.UserInputs());
+	}
+}
---
+++ Assets/0_Snake_O_Pizza/0_Scripts/Interfaces/IAnimation.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public interface IAnimation 
+{
+	Animator AnimPlayer { get; }
+	void SetVelue();
+}
