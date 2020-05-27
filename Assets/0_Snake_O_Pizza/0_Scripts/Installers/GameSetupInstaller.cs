7ca0460b71e5a036f73455e52d9e2f136ad71129
DevJeeth <konerirajb@yahoo.com>
2020-05-27 18:22:41 +0530

    +Registered Player to container, need tospawn player through Zinject API

--- Assets/0_Snake_O_Pizza/0_Scripts/Installers/GameSetupInstaller.cs
+++ Assets/0_Snake_O_Pizza/0_Scripts/Installers/GameSetupInstaller.cs
@@ -3,10 +3,14 @@ using Zenject;
 
 public class GameSetupInstaller : MonoInstaller
 {
+	public GameObject m_gPlayerSnake;
+
     public override void InstallBindings()
     {
 		Container.BindInterfacesAndSelfTo<SteeringWheel>().FromComponentInHierarchy().AsSingle();
 		Container.BindInterfacesAndSelfTo<TilesGenerator>().FromComponentInHierarchy().AsSingle();
 		Container.BindInterfacesAndSelfTo<WallGenerator>().FromComponentInHierarchy().AsSingle();
-    }
+
+		Container.BindFactory<Vector3, Player, Player.PlayerFactory>().FromComponentInNewPrefab(m_gPlayerSnake);
+	}
 }
